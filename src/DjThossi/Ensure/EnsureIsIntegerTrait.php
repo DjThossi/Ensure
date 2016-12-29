d3d40d9b5be8c2bda82c5c0f5c7415eb9a22c83f
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-12-29 07:12:02 +0100

    Introduce EnsureIsIntegerTrait

---
+++ src/DjThossi/Ensure/EnsureIsIntegerTrait.php
@@ -0,0 +1,21 @@
+<?php
+namespace DjThossi\Ensure;
+
+trait EnsureIsIntegerTrait
+{
+    /**
+     * @param string $fieldName
+     * @param mixed $valueToTest
+     * @param int $exceptionCode
+     *
+     * @throws InvalidValueException
+     */
+    protected function ensureIsInteger($fieldName, $valueToTest, $exceptionCode = 0)
+    {
+        if (!is_int($valueToTest)) {
+            $type = is_object($valueToTest) ? get_class($valueToTest) : gettype($valueToTest);
+            $message = sprintf('%s is not an integer, got "%s"', $fieldName, $type);
+            throw new InvalidValueException($message, $exceptionCode);
+        }
+    }
+}
---
+++ tests/Unit/DjThossi/Ensure/EnsureIsIntegerTraitTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace Unit\DjThossi\Ensure;
+
+use DjThossi\Ensure\EnsureIsIntegerTrait;
+use DjThossi\Ensure\InvalidValueException;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+/**
+ * @covers \DjThossi\Ensure\EnsureIsIntegerTrait
+ */
+class EnsureIsIntegerTraitTest extends PHPUnit_Framework_TestCase
+{
+    use EnsureIsIntegerTrait;
+
+    /**
+     * @dataProvider workingValuesProvider
+     *
+     * @param string $fieldName
+     * @param mixed $value
+     */
+    public function testEnsureIsWorking($fieldName, $value)
+    {
+        $this->ensureIsInteger($fieldName, $value);
+        $this->assertTrue(true, 'This test should get here');
+    }
+
+    /**
+     * @return array
+     */
+    public function workingValuesProvider()
+    {
+        return [
+            '1337' => ['FieldName1337', 1337],
+            '0' => ['FieldName0', 0],
+            '-1337' => ['FieldName-1337', -1337],
+        ];
+    }
+
+    /**
+     * @dataProvider failingValuesProvider
+     *
+     * @param string $fieldName
+     * @param mixed $value
+     * @param int $exceptionCode
+     * @param string $expectedMessage
+     */
+    public function testEnsureIsFailing($fieldName, $value, $exceptionCode, $expectedMessage)
+    {
+        $this->expectException(InvalidValueException::class);
+        $this->expectExceptionCode($exceptionCode);
+        $this->expectExceptionMessage($expectedMessage);
+
+        $this->ensureIsInteger($fieldName, $value, $exceptionCode);
+    }
+
+    /**
+     * @return array
+     */
+    public function failingValuesProvider()
+    {
+        return [
+            'String' => ['FieldName', 'Hello World', 1, 'FieldName is not an integer, got "string"'],
+            'Double' => ['FieldName', 1.337, 2, 'FieldName is not an integer, got "double"'],
+            'True' => ['FieldName', true, 3, 'FieldName is not an integer, got "boolean"'],
+            'False' => ['FieldName', false, 4, 'FieldName is not an integer, got "boolean"'],
+            'object' => ['FieldName', new stdClass(), 5, 'FieldName is not an integer, got "stdClass"'],
+            'empty' => ['FieldName', '', 6, 'FieldName is not an integer, got "string"'],
+        ];
+    }
+}
